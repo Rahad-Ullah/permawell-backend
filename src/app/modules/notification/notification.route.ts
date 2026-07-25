e20eaafec0d4518c843013e9df3f5fad136ab0c9
Rahad Ullah <rahadullah10@gmail.com>
2026-07-25 15:19:16 +0600

    feat(notification): notification endpoints

--- src/app/modules/notification/notification.route.ts
+++ src/app/modules/notification/notification.route.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { NotificationController } from './notification.controller';
 import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { NotificationValidation } from './notification.validation';
 
 const router = express.Router();
 
@@ -8,7 +10,12 @@ const router = express.Router();
 router.get('/me', auth(), NotificationController.getMyNotifications);
 
 // read single notification by id
-router.patch('/read/:id', auth(), NotificationController.readNotification);
+router.patch(
+    '/read/:id',
+    auth(),
+    validateRequest(NotificationValidation.readNotificationSchema),
+    NotificationController.readNotification
+);
 
 // read all notifications
 router.patch('/read-all', auth(), NotificationController.readAllNotifications);
