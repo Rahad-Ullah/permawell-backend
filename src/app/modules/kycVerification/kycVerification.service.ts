25e3832637265f58b5e3f14ec334614d2f334320
Rahad Ullah <rahadullah10@gmail.com>
2026-08-06 14:37:06 +0600

    perf(kyc): admin notification on kyc update

--- src/app/modules/kycVerification/kycVerification.service.ts
+++ src/app/modules/kycVerification/kycVerification.service.ts
@@ -4,6 +4,10 @@ import { IKycVerification } from './kycVerification.interface';
 import { KycVerification } from './kycVerification.model';
 import deleteS3File from '../../../shared/deleteS3File';
 import QueryBuilder from '../../builder/QueryBuilder';
+import { sendNotifications } from '../../../helpers/notificationHelper';
+import { NotificationType } from '../notification/notification.constant';
+import { User } from '../user/user.model';
+import { UserRole } from '../user/user.constant';
 
 // --------------- create kyc verification service ----------------
 const createKycVerification = async (payload: IKycVerification): Promise<IKycVerification> => {
@@ -36,6 +40,19 @@ const createKycVerification = async (payload: IKycVerification): Promise<IKycVer
       await deleteS3File(document)
     }
   }
+
+  // send notification to admins
+  const admins = await User.find({ role: { $in: [UserRole.SuperAdmin, UserRole.Admin] } }).select('_id').lean();
+
+  admins.forEach(admin => {
+    sendNotifications({
+      type: NotificationType.KycRequest,
+      receiver: admin._id,
+      title: 'New Verification Request',
+      message: `New kyc verification request is waiting for your approval.`,
+      referenceId: result._id.toString(),
+    }).catch(err => console.error(err));
+  });
 
   return result;
 };
